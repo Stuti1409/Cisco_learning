30cacd7ddf96665f1596ea2e40c8e72f2482ba63
Stuti1409 <stutisharma1409@gmail.com>
2025-09-15 09:17:05 +0530

    Python Learning

---
+++ Day2/flight_app_pickle_dict/db_pickle.py
@@ -0,0 +1,15 @@
+import pickle 
+import os
+
+def read_from_file(filename = 'db1.dat'):
+    if not os.path.exists(filename):
+        flights = []
+        return flights 
+    
+    with open(filename, 'rb') as reader:
+        flights = pickle.load(reader)
+        return flights 
+    
+def write_to_file(flights, filename = 'db.dat'):
+    with open(filename, 'wb') as writer:
+        pickle.dump(flights, writer) 
